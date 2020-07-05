bad75ba76d6bf67082219fce30c0e219908fb2f6
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-05 22:10:34 +0200

    Add primary button prop

--- app/mobile/components/button.component.js
+++ app/mobile/components/button.component.js
@@ -1,7 +1,14 @@
-export default ({ title, type, onClick, loading, children }) => (
+export default ({ title, type, onClick, loading, children, primary }) => (
 	<button onClick={onClick} type={type}>
 		{children}
 		{title}
 		{loading && 'loading'}
+		<style jsx>{`
+      button {
+        color: ${primary ? 'var(--color-primary-100)' : 'var(--color-primary-300)'};
+        background: ${primary ? 'var(--color-primary-500)' : 'var(--color-white)'};
+        border: 1px solid ${primary ? 'var(--color-primary-500)' : 'var(--color-primary-300)'};
+      }
+    `}</style>
 	</button>
 )
