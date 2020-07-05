a9f00a77d0f9d661d9c72717b1beb9f3d200e34f
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-05 18:37:17 +0200

    Add temporary navigation

--- app/mobile/pages/index.js
+++ app/mobile/pages/index.js
@@ -1,5 +1,20 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import Link from 'next/link'
+import { Layout } from '../components'
 
-export default () => <Layout wrap row><h1>Welcome</h1></Layout>
+/* TODO: routes from constant */
+export default () => <Layout wrap row>
+	<h1>Welcome</h1>
+	<div>
+		<Link href="/auth/signin"><a>Get started</a></Link>
+	</div>
+	<style jsx>
+		{`
+      div {
+        margin-top: auto;
+        padding: 1.6rem 0;
+        border-top: 1px solid #000;
+      }
+    `}
+	</style>
+</Layout>
 
