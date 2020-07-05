751750e6a3007100b1b9e78d894f87fde6b8c8c0
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-05 19:32:36 +0200

    Add input component

---
+++ app/mobile/components/input.component.js
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types'
+
+const Input = ({ type, name, placeholder, required, error, value, icon, ...rest }) => (
+	<div className="input">
+		{icon && (
+			<div className="icon">
+				<i
+					data-eva={icon}
+					data-eva-animation="pulse"
+					data-eva-hover="true"
+					data-eva-infinite="true"
+					data-eva-fill="#aaaaaa"
+				/>
+			</div>
+		)}
+		<input
+			type={type}
+			name={name}
+			placeholder={placeholder}
+			required={required}
+			value={value}
+			{...rest}
+		/>
+		<style jsx>
+			{`
+        .input {
+          display: flex;
+        }
+
+        input {
+          flex: 1;
+        }
+
+        input::placeholder {
+          color: #aaa;
+          padding-left: ${icon ? '2rem' : '0' };
+        }
+
+        .icon {
+          position: absolute;
+          margin: 1.4rem;
+          margin-bottom: 1.2rem;
+        }
+      `}
+		</style>
+	</div>
+)
+
+Input.propTypes = {
+	type: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	placeholder: PropTypes.string,
+	required: PropTypes.bool,
+	error: PropTypes.string,
+	value: PropTypes.string,
+	icon: PropTypes.string
+}
+
+Input.defaultProps = {
+	icon: null,
+	placeholder: ''
+}
+
+export default Input
