ff3cdda33d886bf181f26b0739f1615057712d93
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-06 23:07:14 +0200

    Add dashboard header

--- app/mobile/pages/dashboard.js
+++ app/mobile/pages/dashboard.js
@@ -1,7 +1,39 @@
-import {Layout} from '../components'
+import {Layout, Header} from '../components'
+import cookie from 'js-cookie'
+import { GoPerson } from 'react-icons/go'
+import Link from 'next/link'
 
 export default () => (
 	<Layout>
-		<h1>Welcome</h1>
+		<Header title="Welcome">
+			{cookie.get('token') && 
+      <Link href="/account">
+      	<a className="avatar">
+      		<span>My account</span>
+      	  <GoPerson/>
+      	</a>
+      </Link>
+			}
+		</Header>
+		<style jsx>
+			{`
+        a {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: var(--color-primary-300);
+          padding: 1.6rem;
+        }
+
+        a > :global(.icon) {
+          height: 2rem;
+          width: 2rem;
+        }
+
+        span {
+          padding: 0.4rem;
+        }
+    `}
+		</style>
 	</Layout>
 )
