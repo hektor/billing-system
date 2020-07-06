8b20b2c18b65fb35f148055489e6934f9341bca7
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-06 17:46:31 +0200

    Re-export new components

--- app/mobile/components/index.js
+++ app/mobile/components/index.js
@@ -1,9 +1,26 @@
+/*
+ * UI
+ */
+
 export {default as Button} from './button.component'
-export {default as Documents} from './documents.component'
 export {default as Dropdown} from './dropdown.component'
 export {default as Dropzone} from './dropzone.component'
 export {default as Form} from './form.component'
 export {default as FormField} from './form-field.component'
+export {default as Header} from './header.component'
+export {default as Icon} from './icon.component'
 export {default as Input} from './input.component'
 export {default as Layout} from './layout.component'
 export {default as Modal} from './modal.component'
+
+/*
+ * Containers
+ */
+
+export {default as Documents} from './documents.component'
+
+/*
+ * Higher order components
+ */
+
+export {default as withSplash} from './with-splash.component'
