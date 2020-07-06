519e66a7ab6dc180857b00ab2a0f1f3960153a78
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-06 19:37:56 +0200

    Replace eva icons with octicons

--- app/mobile/pages/_app.js
+++ app/mobile/pages/_app.js
@@ -1,39 +1,30 @@
-import {useEffect} from 'react'
 import Head from 'next/head'
 import { ApolloProvider } from '@apollo/react-hooks'
 import withData from '../apollo/apollo'
-import * as eva from 'eva-icons'
+import { IconContext } from 'react-icons'
 
+const App = ({ Component, props, apollo }) => (
+	<ApolloProvider client={apollo}>
+		<Head>
+			<meta charSet="utf-8"/>
+			<meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+			<meta name="description" content="Description"/>
+			<meta name="keywords" content="Keywords"/>
+			<title>Page Title</title>
 
-const App = ({ Component, props, apollo }) => { 
-
-	/*
-   * FIXME: Load icons appropriatly
-   */
-	useEffect(() => {
-		eva.replace()
-	})
-
-	return (
-		<ApolloProvider client={apollo}>
-			<Head>
-				<meta charSet="utf-8"/>
-				<meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-				<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
-				<meta name="description" content="Description"/>
-				<meta name="keywords" content="Keywords"/>
-				<title>Page Title</title>
-
-				<meta name="theme-color" content="#ffffff"/>
-				<link rel='manifest' href='/manifest.json' />
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"
-				/>
-				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-			</Head>
+			<meta name="theme-color" content="#ffffff"/>
+			<link rel='manifest' href='/manifest.json' />
+			<link
+				rel="stylesheet"
+				href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"
+			/>
+			<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+		</Head>
+		<IconContext.Provider value={{size: 24, className: 'icon'}}>
 			<Component {...props} />
-		</ApolloProvider>
-	)}
+		</IconContext.Provider>
+	</ApolloProvider>
+)
 
 export default withData(App)
