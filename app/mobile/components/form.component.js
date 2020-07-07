217f285642a5695c7a5cb4e5030bb2d8fd0785aa
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-07 22:52:27 +0200

    Show response errors

--- app/mobile/components/form.component.js
+++ app/mobile/components/form.component.js
@@ -1,24 +1,14 @@
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useForm } from '../hooks'
 import { FormField } from '../components'
 
-const Form = ({ fields, children, onSubmit }) => {
-	const { onChange, values, errors, isValid, onSubmit: onFormSubmit, showFeedback } = useForm(fields)
+const Form = ({ fields, children, onSubmit, generalErrors }) => {
+	const { onChange, values, errors, setErrors, onSubmit: onFormSubmit, showFeedback, setShowFeedback } = useForm(fields)
 
-	/*
-   * Render fields from fields prop
-   */
-	const renderFields = () =>
-		fields.map((field) => (
-			<FormField
-				key={field.name}
-				{...field}
-				setField={onChange}
-				showFeedback={showFeedback}
-				value={values[field.name]}
-				error={errors[field.name]}
-			/>
-		))
+	useEffect(() => {
+		if(generalErrors) setShowFeedback(false)
+	}, [])
 
 	/*
    * Pass form values to parent's onSubmit
@@ -31,7 +21,17 @@ const Form = ({ fields, children, onSubmit }) => {
 
 	return (
 		<form onSubmit={handleSubmit} noValidate>
-			{fields && renderFields()}
+			{fields && 
+        fields.map(field => (
+        	<FormField
+        		key={field.name}
+        		{...field}
+        		setField={onChange}
+        		value={values[field.name]}
+        		error={errors[field.name]}
+        		showFeedback={showFeedback}
+        	/>))
+			}
 			{children}
 		</form>
 	)
