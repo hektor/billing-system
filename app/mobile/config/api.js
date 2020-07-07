0943762b77dc7f1f53efff9f13dc8dfa3d422d17
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-07 22:59:38 +0200

    Add password recovery routes

--- app/mobile/config/api.js
+++ app/mobile/config/api.js
@@ -5,5 +5,7 @@ const { BASE_URL } = env
 export default {
 	GRAPHQL: `${BASE_URL}graphql`,
 	SIGNIN: `${BASE_URL}auth/local`,
-	SIGNUP: `${BASE_URL}auth/local/register`
+	SIGNUP: `${BASE_URL}auth/local/register`,
+	FORGOT_PASSWORD: `${BASE_URL}auth/forgot-password`,
+	RESET_PASSWORD: `${BASE_URL}auth/reset-password`
 }
