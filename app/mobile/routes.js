a696505ba098a5b05b7f6231a10b6e7a375f3be5
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-07 23:00:22 +0200

    Add recover password routes

--- app/mobile/routes.js
+++ app/mobile/routes.js
@@ -1,3 +1,4 @@
-export const SIGNUP= 'auth/signup'
-export const SIGNIN= 'auth/signin'
-export const DASHBOARD= 'dashboard'
+export const SIGNUP= '/auth/signup'
+export const SIGNIN= '/auth/signin'
+export const DASHBOARD= '/dashboard'
+export const RECOVER_PASSWORD = '/recover-password'
