1a57251f130128694c5ac87aedb2c4a5de475648
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-07 22:54:54 +0200

    Re-export 'recover password' form data

--- app/mobile/data/index.js
+++ app/mobile/data/index.js
@@ -1,3 +1,4 @@
 export {default as signupForm} from './signupForm'
 export {default as signinForm} from './signinForm'
 export {default as settingsForm} from './settingsForm'
+export {default as recoverPasswordForm} from './recoverPasswordForm'
