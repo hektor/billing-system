049d7a55ab60ca526690cbf249a920b62ab7cbc8
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-17 17:21:50 +0200

    Add 'clients' component

---
+++ app/mobile/components/clients.component.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import Query from '../apollo/query'
+import { GET_CLIENTS } from '../apollo'
+
+export default () => (
+	<div className="list">
+		<Query query={GET_CLIENTS} id={null}>
+			{({data: {clients}}) =>
+				clients.map(({ id, name, phone }) => (
+					<Link key={id} href={{pathname: 'log', query: id }}>
+						<div className="item">
+							<span>{name}</span>
+							<span>{phone}</span>
+						</div>
+					</Link>
+				))
+			}
+		</Query>
+		<style jsx>
+			{`
+        .item {
+          display: flex;
+          justify-content: space-between;
+          padding: 3.2rem 1.6rem;
+          border-bottom: 1px solid var(--color-primary-300);
+        }
+      `}
+		</style>
+	</div>
+)
