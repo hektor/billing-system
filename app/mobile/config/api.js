deb04ae9454a7e3bf332010b90487241e3579f10
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-17 20:37:28 +0200

    Remove / from base url

--- app/mobile/config/api.js
+++ app/mobile/config/api.js
@@ -3,9 +3,10 @@ import { env } from '.'
 const { BASE_URL } = env
 
 export default {
-	GRAPHQL: `${BASE_URL}graphql`,
-	SIGNIN: `${BASE_URL}auth/local`,
-	SIGNUP: `${BASE_URL}auth/local/register`,
-	FORGOT_PASSWORD: `${BASE_URL}auth/forgot-password`,
-	RESET_PASSWORD: `${BASE_URL}auth/reset-password`
+	GRAPHQL: `${BASE_URL}/graphql`,
+	GET_USER: `${BASE_URL}/users/me`,
+	SIGNIN: `${BASE_URL}/auth/local`,
+	SIGNUP: `${BASE_URL}/auth/local/register`,
+	FORGOT_PASSWORD: `${BASE_URL}/auth/forgot-password`,
+	RESET_PASSWORD: `${BASE_URL}/auth/reset-password`
 }
