52752dcdfe962a59f8607d64ceaeb8594d6f739e
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-18 00:12:03 +0200

    Set up client detail page

--- app/mobile/containers/client.container.js
+++ app/mobile/containers/client.container.js
@@ -1,12 +1,20 @@
 import {useRouter} from 'next/router'
 import Query from '../apollo/query'
 import { GET_CLIENT } from '../apollo'
+import { Header } from '../components'
 
 export default () => (
 	<Query query={GET_CLIENT} id={useRouter().query.id}>
 		{({client}) => {
-			const {id} = client
-			return id
+			const {name, phone, address, logs} = client
+			return (
+				<div className="client">
+					<Header title={name} />
+					<span>{phone}</span>
+					<span>{JSON.stringify(address)}</span>
+					<span>{JSON.stringify(logs)}</span>
+				</div>
+			)
 		}}
 	</Query>
 )
