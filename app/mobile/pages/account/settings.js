5a4b1c264ae6e6ace5f0bd6b97f0b3098ac26cf5
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-22 20:00:15 +0200

    Replace icons

--- app/mobile/pages/account/settings.js
+++ app/mobile/pages/account/settings.js
@@ -1,19 +1,17 @@
-import {Layout, Header, Form, Button, GoBack} from '../../components'
-import {GoCheck} from 'react-icons/go'
+import {Layout, Header, Form, Button} from '../../components'
+import {RiSave3Line} from 'react-icons/ri'
 import {settingsForm} from '../../data'
 
 export default () => {
 	const handleSubmit = e => console.log(e)
 	return (
 		<Layout>
-			<Header title="Account settings">
-				<GoBack/>
-			</Header>
+			<Header title="Account settings" />
 			<Form
 				fields={settingsForm.fields}
 				onSubmit={handleSubmit}
 			>
-				<Button primary type="submit" icon={<GoCheck/>}>Save changes</Button>
+				<Button primary type="submit" icon={<RiSave3Line/>}>Save changes</Button>
 			</Form>
 		</Layout>
 	)
