3b0bf391d51a7781cd5ddf49e43ef02e3cedc0aa
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-22 11:04:49 +0200

    Add go back button

--- app/mobile/pages/logs/[id].js
+++ app/mobile/pages/logs/[id].js
@@ -1,8 +1,9 @@
-import {Layout} from '../../components'
+import {Layout, GoBack} from '../../components'
 import {Log} from '../../containers'
 
 export default () => (
 	<Layout bottomNav>
 		<Log />
+		<GoBack title="Logs overview"/>
 	</Layout>
 )
