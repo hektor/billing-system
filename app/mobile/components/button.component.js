ec8a6aca8e917a9b767cac38c7598871681f33e8
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-22 19:55:09 +0200

    Style button hover state

--- app/mobile/components/button.component.js
+++ app/mobile/components/button.component.js
@@ -16,6 +16,13 @@ export default ({ title, type, onClick, loading, children, primary, icon }) => (
         color: ${primary ? 'var(--color-primary-100)' : 'var(--color-primary-300)'};
         background: ${primary ? 'var(--color-primary-500)' : 'var(--color-white)'};
         border: 1px solid ${primary ? 'var(--color-primary-500)' : 'var(--color-primary-300)'};
+        transition: .16s;
+      }
+
+      button:hover {
+        color: ${primary ? 'var(--color-primary-300)' : 'var(--color-primary-500)'};
+        background: ${primary ? 'var(--color-primary-700)' : 'var(--color-primary-200)'};
+        border-color: ${primary ? 'var(--color-primary-700)' : 'var(--color-primary-500)'};
       }
     `}</style>
 	</button>
