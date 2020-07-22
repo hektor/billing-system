cd93e8194c88f98376170d72dfecb202982d91e9
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-22 19:58:23 +0200

    Add list header

--- app/mobile/containers/logs.container.js
+++ app/mobile/containers/logs.container.js
@@ -1,3 +1,4 @@
+import {RiTimeLine, RiContactsBook2Line} from 'react-icons/ri'
 import Query from '../apollo/query'
 import { GET_LOGS } from '../apollo'
 import { formatDate } from '../utils/date'
@@ -5,6 +6,16 @@ import { ListItem } from '../components'
 
 export default () => (
 	<div className="list">
+		<div className="list-header">
+			<div className="header-item client">
+				<RiContactsBook2Line />
+				<span>Client</span>
+			</div>
+			<div className="header-item date">
+				<RiTimeLine />
+				<span>Date</span>
+			</div>
+		</div>
 		<Query query={GET_LOGS} id={null}>
 			{({logs}) =>
 				logs.map(({id, client_id, startTime}) => (
@@ -15,5 +26,37 @@ export default () => (
 				))
 			}
 		</Query>
+		<style jsx>
+			{`
+        .list-header {
+          display: flex;
+          color: var(--color-primary-700);
+        }
+
+        .list-header > * {
+          flex: 1;
+          display: flex;
+          align-items: center;
+          padding: 1.6rem;
+          border-bottom: 1px solid var(--color-primary-100);
+        }
+
+        .header-item:first-child {
+          margin-right: 0.8rem;
+        }
+
+        .header-item:last-child {
+          margin-left: 0.8rem;
+        }
+
+        .list-header span {
+          margin-left: 0.4rem;
+        }
+
+        .date {
+          justify-content: flex-end;
+        }
+      `}
+		</style>
 	</div>
 )
