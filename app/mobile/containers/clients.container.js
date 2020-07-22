6d9ffc207b62dd038243fc434b7ece8c51317c69
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-22 12:24:06 +0200

    Use 'list item' component

--- app/mobile/containers/clients.container.js
+++ app/mobile/containers/clients.container.js
@@ -1,30 +1,18 @@
-import Link from 'next/link'
 import Query from '../apollo/query'
 import { GET_CLIENTS } from '../apollo'
+import { ListItem } from '../components'
 
 export default () => (
 	<div className="list">
 		<Query query={GET_CLIENTS} id={null}>
 			{({clients}) =>
 				clients.map(({id, name, phone}) => (
-					<Link key={id} href={`/clients/${id}`}>
-						<div className="item">
-							<span>{name}</span>
-							<span>{phone}</span>
-						</div>
-					</Link>
+					<ListItem key={id} href={`/clients/${id}`}>
+						<span>{name}</span>
+						<span>{phone}</span>
+					</ListItem>
 				))
 			}
 		</Query>
-		<style jsx>
-			{`
-        .item {
-          display: flex;
-          justify-content: space-between;
-          padding: 3.2rem 1.6rem;
-          border-bottom: 1px solid var(--color-primary-300);
-        }
-      `}
-		</style>
 	</div>
 )
