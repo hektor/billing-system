d027adb7a855b4293a71e86b53cca6ffe8995d65
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-24 00:57:35 +0200

    Refactor

--- app/mobile/pages/logs/create.js
+++ app/mobile/pages/logs/create.js
@@ -1,18 +1,16 @@
-import {useRouter} from 'next/router'
+import Router from 'next/router'
 import {RiCloseLine} from 'react-icons/ri'
 import {Layout, Header, Form, BottomNav, Button} from '../../components'
 import {createLogForm as form}  from '../../data'
 
 export default () => { 
 	const handleSubmit = async e => console.log(e)
-	const router = useRouter()
-
 	return (
 		<Layout>
 			<Header title="Create new log" />
 			<Form fields={form.fields} onSubmit={handleSubmit}/>
 			<BottomNav>
-				<Button onClick={() => router.replace('/logs')} title="Cancel" icon={<RiCloseLine />} />
+				<Button onClick={() => Router.replace('/logs')} title="Cancel" icon={<RiCloseLine />} />
 			</BottomNav>
 			<style jsx>
 				{`
