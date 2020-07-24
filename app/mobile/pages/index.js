4a904508006d8185950b34d7d629451088314c08
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-24 12:22:47 +0200

    Use index page as splash screen

---
+++ app/mobile/pages/index.js
@@ -0,0 +1,15 @@
+import {useContext, useEffect} from 'react'
+import Router from 'next/router'
+import {Spinner} from '../components'
+import {AuthCtx} from './_app'
+
+export default () => { 
+	const {user} = useContext(AuthCtx) 
+
+	useEffect(() => {
+		if(user) Router.replace('/dashboard')
+		if(!user) Router.replace('/auth/signin')
+	}, [])
+
+	return <Spinner/> 
+}
