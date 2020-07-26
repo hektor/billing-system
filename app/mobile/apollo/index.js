2edb4ffd8488d61b542c8b6008a882d3448520e6
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-26 19:49:09 +0200

    Re-export 'latest log' query

--- app/mobile/apollo/index.js
+++ app/mobile/apollo/index.js
@@ -12,6 +12,7 @@ export {default as Query} from './query'
  */
 
 export {default as GET_LOG} from './get-log.query'
+export {default as GET_LATEST_LOG} from './get-latest-log.query'
 export {default as GET_LOGS} from './get-logs.query'
 export {default as CREATE_LOG} from './create-log.query'
 export {default as DELETE_LOG} from './delete-log.query'
