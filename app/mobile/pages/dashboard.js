3612d77ba5b69d2e73cf7ec5d3d52318ce90765b
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-26 19:44:25 +0200

    Render latest log

--- app/mobile/pages/dashboard.js
+++ app/mobile/pages/dashboard.js
@@ -1,8 +1,10 @@
+import {Query, GET_LATEST_LOG} from '../apollo'
 import Link from 'next/link'
 import {RiAccountCircleLine} from 'react-icons/ri'
 import {useContext} from 'react'
 import {AuthCtx} from './_app'
 import {Layout, Header, BottomNav} from '../components'
+import {formatDate, formatTime} from '../utils/date'
 
 /*
  * Overview landing page for authenticated users
@@ -10,7 +12,8 @@ import {Layout, Header, BottomNav} from '../components'
 
 export default () => {
 	const {user} = useContext(AuthCtx)
-	const getGreeting = () => user && user.name ? user.name : 'Welcome'
+	const getGreeting = () => user && user.name ? 'Welcome' + user.name : 'Welcome'
+
 	return(
 		<Layout bottomNav>
 			<Header title={getGreeting()}>
@@ -21,6 +24,16 @@ export default () => {
 					</a>
 				</Link>
 			</Header>
+			<p>Today is {formatDate(new Date())}</p>
+			{user && formatDate(user.lastSeen)}
+			<Query query={GET_LATEST_LOG} id={null}>
+				{({logs}) => {
+					const {startTime, endTime, totalBreakDuration} = logs[0]
+					return (
+						<p>Your latest work activity was on {formatDate(startTime)}, from {formatTime(startTime)} until {formatTime(endTime)}.</p>
+					)
+				}}
+			</Query>
 			<BottomNav/>
 			<style jsx>
 				{`
