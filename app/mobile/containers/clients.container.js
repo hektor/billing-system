6f2179ee4fee7ea989c893a2ae1bf382a4021067
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-29 19:54:27 +0200

    Show icons based on available information

--- app/mobile/containers/clients.container.js
+++ app/mobile/containers/clients.container.js
@@ -1,22 +1,31 @@
 import {Query, GET_CLIENTS} from '../apollo'
 import {ListItem} from '../components'
 import {CLIENTS} from '../routes'
+import {RiPhoneLine, RiBuildingLine} from 'react-icons/ri'
 
 /*
  * List of clients
  */
 
 export default () => (
-	<div className="list">
-		<Query query={GET_CLIENTS} id={null}>
-			{({clients}) =>
-        clients.map(({id, name, phone}) => (
+  <div className='list'>
+    <Query query={GET_CLIENTS} id={null}>
+      {({clients}) =>
+        clients.map(({id, name, phone, address}) => (
           <ListItem key={id} href={`${CLIENTS}/${id}`}>
-						<span>{name}</span>
-						<span>{phone}</span>
-					</ListItem>
-				))
-			}
-		</Query>
-	</div>
+            <span>{name}</span>
+            <div className='actions'>
+              {!!phone && <RiPhoneLine />}
+              {!!address && <RiBuildingLine />}
+            </div>
+          </ListItem>
+        ))
+      }
+    </Query>
+    <style jsx>{`
+      .actions > :global(*) {
+        margin: 0 0.4rem;
+      }
+    `}</style>
+  </div>
 )
