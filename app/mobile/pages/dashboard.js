e64ee192a8e13e264523d91d70101455551e00cd
Hektor Misplon <hektor.misplon@gmail.com>
2020-07-29 14:36:07 +0200

    Render latest activity

--- app/mobile/pages/dashboard.js
+++ app/mobile/pages/dashboard.js
@@ -4,7 +4,7 @@ import {RiAccountCircleLine} from 'react-icons/ri'
 import {useContext} from 'react'
 import {AuthCtx} from './_app'
 import {Layout, Header, BottomNav} from '../components'
-import {formatDate, formatTime} from '../utils/date'
+import {now, formatDate, formatTime, diffDays} from '../utils/date'
 import {ACCOUNT} from '../routes'
 
 /*
@@ -12,49 +12,56 @@ import {ACCOUNT} from '../routes'
  */
 
 export default () => {
-	const {user} = useContext(AuthCtx)
-	const getGreeting = () => user && user.name ? 'Welcome' + user.name : 'Welcome'
+  const {user} = useContext(AuthCtx)
+  const getGreeting = () =>
+    user && user.name ? 'Welcome' + user.name : 'Welcome'
 
-	return(
-		<Layout bottomNav>
+  return (
+    <Layout bottomNav>
       <Header title={getGreeting()}>
         <Link href={ACCOUNT}>
-					<a className="avatar">
-						<span>My account</span>
-						<RiAccountCircleLine size="32"/>
-					</a>
-				</Link>
-			</Header>
-			<p>Today is {formatDate(new Date())}</p>
-			{user && formatDate(user.lastSeen)}
-			<Query query={GET_LATEST_LOG} id={null}>
-        {({logs}) => {
-          const {startTime, endTime} = logs[0]
-					return (
-						<p>Your latest work activity was on {formatDate(startTime)}, from {formatTime(startTime)} until {formatTime(endTime)}.</p>
-					)
-				}}
-			</Query>
-			<BottomNav/>
-			<style jsx>
-				{`
-        a {
-          display: flex;
-          align-items: center;
-          color: var(--color-primary-300);
-          padding: 1.6rem;
-          transition: .16s;
-        }
+          <a className='avatar'>
+            <span>My account</span>
+            <RiAccountCircleLine size='32' />
+          </a>
+        </Link>
+      </Header>
+      {/* user && formatDate(user.lastSeen) */}
+      <h2>Latest activity</h2>
+      <div className='card'>
+        <Query query={GET_LATEST_LOG} id={null}>
+          {({logs}) => {
+            const {startTime, endTime} = logs[0]
+            return (
+              <>
+                <h3>{`${diffDays(now, startTime)} Days ago`}</h3>
+                Latest work activity {formatDate(startTime)}, from{' '}
+                {formatTime(startTime)} until {formatTime(endTime)}.
+              </>
+            )
+          }}
+        </Query>
+      </div>
+      <BottomNav />
+      <style jsx>
+        {`
+          a {
+            display: flex;
+            align-items: center;
+            color: var(--color-primary-300);
+            padding: 1.6rem;
+            transition: 0.16s;
+          }
 
-        a:hover {
-          color: var(--color-primary-500);
-        }
+          a:hover {
+            color: var(--color-primary-500);
+          }
 
-        span {
-          margin-right: 0.8rem;
-        }
-    `}
-			</style>
-		</Layout>
-	)
+          span {
+            margin-right: 0.8rem;
+          }
+        `}
+      </style>
+    </Layout>
+  )
 }
