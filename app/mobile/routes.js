7d6e01a283ff0a1ecf3277afbf600fc97d872d6b
Hektor Misplon <hektor.misplon@gmail.com>
2020-08-20 15:34:30 +0200

    Formatting

--- app/mobile/routes.js
+++ app/mobile/routes.js
@@ -2,9 +2,9 @@
  * Client-side route constants
  */
 
-export const SIGNUP= '/auth/signup'
-export const SIGNIN= '/auth/signin'
-export const RECOVER_PASSWORD= '/auth/recover-password'
+export const SIGNUP = '/auth/signup'
+export const SIGNIN = '/auth/signin'
+export const RECOVER_PASSWORD = '/auth/recover-password'
 export const RESET_PASSWORD = '/auth/reset-password'
 
 export const DASHBOARD = '/dashboard'
