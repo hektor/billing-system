0be7015ece36e19286746d4db6f4414cc240dd19
Hektor Misplon <hektor.misplon@gmail.com>
2020-08-22 15:56:20 +0200

    Set up svelte routing

--- app/web/src/App.svelte
+++ app/web/src/App.svelte
@@ -1,14 +1,39 @@
 <script>
+  import { Router, Route } from "svelte-routing";
+
+  import Logs from "./routes/Logs.svelte";
+  import Log from "./routes/Log.svelte";
+
   import ApolloClient from "apollo-boost";
   import { setClient } from "svelte-apollo";
-  import Logs from "./containers/Logs.svelte";
 
-  const endpoint = window.location.pathname.split("/");
-
-  $: id = endpoint[1] === "" ? null : Number(endpoint[1]);
+  export let url = "";
 
   const client = new ApolloClient({ uri: "http://localhost:1337/graphql" });
   setClient(client);
 </script>
 
-<Logs {id} />
+<style>
+  nav {
+    padding: 1.6rem;
+    color: var(--color-primary-100);
+    background: var(--color-primary-500);
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+</style>
+
+<div>
+  <nav>My logs</nav>
+  <Router {url}>
+    <Route path="clients/:clientId" component={Logs} let:params />
+    <Route path="clients/:clientId/logs/:logId" component={Log} let:params />
+    <Route path="/">
+      <h1>Home</h1>
+    </Route>
+  </Router>
+</div>
